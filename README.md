f5108c6fed37bf16d820fe42015b98d90123b790
Mustafa Musaji <mmusaji@mmusaji.fab.csb>
2016-08-31 14:44:36 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Leaderboard
+Show the highest scores for the mobile game for the Summit Keynote.
+
+Installation:
+
+```
+cd leaderboard/
+npm install
+```
+
+There are three ways in which you can build and run the web app:
+
+* Build once:
+    * `npm run build`
+    * Open `index.html`
+* Watch files continuously, rebuild incrementally, whenever one of them changes:
+    * `npm run watch`
+    * Open `index.html`, manually reload page in browser whenever there was a change
+* Hot reloading server:
+    * `npm start`
+    * Chrome should open automatically and go to `http://localhost:8080/`, page reloads automatically when there are changes
+    * (if it doesn't reload, make sure you save it)
---
+++ js/main.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var CHANGE_ANIMATION_DURATION = 20; // ms
+
+//imports
+var Ractive = require('ractive');
+var ReconnectingWebSocket = require('reconnectingwebsocket');
+var suffixes = require('./nth-suffix');
+var _ = require('lodash');
+
+var data = {
+    leaders: [],
+    suffixes: suffixes,
+};
+
+var ractive = new Ractive({
+    el: document.body,
+    template: document.querySelector('#template-leaderboard').textContent,
+    data: data,
+});
+
+window.ractive = ractive; // for debugging
+window.data = data;
+
+
+ractive.observe('leaders.*.username', function (newValue, oldValue, keypath, i, key) {
+    // this leader changed, triggers a css anim
+    ractive.set(`leaders[${i}].changed`, false);
+    var timeStart = (new Date()).getTime();
+    // remove changed flag after anim has ended
+    setTimeout(function removeChanged() {
+        ractive.set(`leaders[${i}].changed`, true);
+        var timeEnd = (new Date()).getTime();
+        console.log(`.change removed after ${timeEnd - timeStart} ms`);
+    }, CHANGE_ANIMATION_DURATION);
+});
+
+function setLeaders(leaders) {
+    var sorted_leaders = _.sortBy(leaders, 'score').reverse();
+    ractive.set('leaders', sorted_leaders);
+}
+
+/*--- websocket init and handlers ---*/
+
+//var ws = new ReconnectingWebSocket('ws://localhost:9001/leaderboard');
+//var ws = new ReconnectingWebSocket('ws://gamebus-boards-production.apps-test.redhatkeynote.com/leaderboard');
+var ws = new ReconnectingWebSocket('ws://localhost:9004/leaderboard');
+ws.onopen = function wsOpen() { console.log('websocket connection open'); };
+ws.onclose = function wsClose() { console.log('websocket connection closed'); };
+ws.onmessage = function wsMessage(msg) {
+    try {
+        var payload = JSON.parse(msg.data);
+        if (payload.length) {
+            setLeaders(payload);
+        }
+    } catch (e) {
+        console.error(e);
+    }
+};
