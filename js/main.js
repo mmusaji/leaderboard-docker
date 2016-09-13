135ad19afe0d64b5981718173e18a016d0ca5fe6
Mustafa Musaji <mmusaji@mmusaji.fab.csb>
2016-09-13 12:45:45 +0100

    Once again, corrected the url to the ws endpoint to the game-server

--- js/main.js
+++ js/main.js
@@ -44,8 +44,8 @@ function setLeaders(leaders) {
 
 //var ws = new ReconnectingWebSocket('ws://localhost:9001/leaderboard');
 //var ws = new ReconnectingWebSocket('ws://gamebus-boards-production.apps-test.redhatkeynote.com/leaderboard');
-// var ws = new ReconnectingWebSocket('ws://localhost:9004/leaderboard');
-var ws = new ReconnectingWebSocket('ws://game-server-lb:9004/leaderboard');
+//var ws = new ReconnectingWebSocket('ws://localhost:9004/leaderboard');
+var ws = new ReconnectingWebSocket('ws://game-server-lb-forum.apps.redhatforum.co.uk/leaderboard');
 ws.onopen = function wsOpen() { console.log('websocket connection open'); };
 ws.onclose = function wsClose() { console.log('websocket connection closed'); };
 ws.onmessage = function wsMessage(msg) {
