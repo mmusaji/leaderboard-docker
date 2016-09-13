e9312c7b2f386076622f79b900329bd494569a76
Mustafa Musaji <mmusaji@mmusaji.fab.csb>
2016-09-13 11:51:48 +0100

    Fixed new url for game server for new environment on master.redhatforum.co.uk

--- js/main.js
+++ js/main.js
@@ -45,7 +45,7 @@ function setLeaders(leaders) {
 //var ws = new ReconnectingWebSocket('ws://localhost:9001/leaderboard');
 //var ws = new ReconnectingWebSocket('ws://gamebus-boards-production.apps-test.redhatkeynote.com/leaderboard');
 // var ws = new ReconnectingWebSocket('ws://localhost:9004/leaderboard');
-var ws = new ReconnectingWebSocket('ws://mobile.apps.redhatoforum.co.uk:8000/leaderboard');
+var ws = new ReconnectingWebSocket('ws://game-server-lb:9004/leaderboard');
 ws.onopen = function wsOpen() { console.log('websocket connection open'); };
 ws.onclose = function wsClose() { console.log('websocket connection closed'); };
 ws.onmessage = function wsMessage(msg) {
